d5fbda4f817eb8a9cd9d93d9d3dd9db6e23802e5
Hubert Kecler <hubert.kecler@edu.itspiemonte.it>
2023-10-11 12:10:06 +0200

    caricato tutto il progetto

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { HomeComponent } from "./home/home.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+@NgModule({
+    imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
+    declarations: [AppComponent, TopBarComponent, HomeComponent],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
